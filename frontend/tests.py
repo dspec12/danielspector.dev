9067dbcc784acc7a0465b83e95b9de007072044c
dspec12 <danspec12@gmail.com>
2019-07-21 18:37:08 -0400

    added some css from bootstrap, blog page, contact icons

--- frontend/tests.py
+++ frontend/tests.py
@@ -1,6 +1,5 @@
 from django.test import SimpleTestCase
 
-# Create your tests here.
 class SimpleTests(SimpleTestCase):
     def test_homepage_status_code(self):
         response = self.client.get('/')
@@ -8,4 +7,8 @@ class SimpleTests(SimpleTestCase):
 
     def test_about_status_code(self):
         response = self.client.get('/about/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_blog_status_code(self):
+        response = self.client.get('/blog/')
         self.assertEqual(response.status_code, 200)
