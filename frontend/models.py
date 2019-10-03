eea94cf059e819be0418a060949a843ea4f2800d
dspec12 <danspec12@gmail.com>
2019-10-03 17:31:23 -0400

    Added "pretty" urls to blog links

--- frontend/models.py
+++ frontend/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from datetime import datetime
+from autoslug import AutoSlugField
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=20)
-    description = models.CharField(max_length=100, default=None)
+    title = models.CharField(max_length=100)
+    slug = AutoSlugField(populate_from="title")
+    description = models.CharField(max_length=200)
     pub_date = models.DateField(default=datetime.today())
     author = models.ForeignKey("auth.User", on_delete=models.CASCADE)
     body = models.TextField()
--- frontend/urls.py
+++ frontend/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import HomePageView, AboutPageView, BlogPageView, BlogDetailView
 
 urlpatterns = [
-    path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
-    path('blog/', BlogPageView.as_view(), name='blog'),
-    path('about/', AboutPageView.as_view(), name='about'),
-    path('', HomePageView.as_view(), name='home'),
+    path("blog/", BlogPageView.as_view(), name="blog"),
+    path("blog/<int:pk>/<slug:slug>/", BlogDetailView.as_view(), name="post_detail"),
+    path("about/", AboutPageView.as_view(), name="about"),
+    path("", HomePageView.as_view(), name="home"),
 ]
